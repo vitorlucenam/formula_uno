e37c62429ac75b14b8fe3e03a4dab03f9755a07b
Vitor Moura <vitorlucenam@gmail.com>
2023-05-06 18:40:19 -0300

    todos os componentes separados e app funcionando

--- App.js
+++ App.js
@@ -1,22 +1,7 @@
 import * as React from 'react';
-
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import HomeScreen from './pages/HomeScreen';
-import MenuScreen from './pages/MenuScreen';
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={MenuScreen} />
-    </Tab.Navigator>
-  );
-}
+import MyTabs from './components/TabsComponent';
 
 export default function App() {
   return (
---
+++ components/TabsComponent.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from '../pages/HomeScreen';
+import MenuScreen from '../pages/MenuScreen';
+
+const Tab = createBottomTabNavigator();
+
+
+const MyTabs = () => (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Menu" component={MenuScreen} />
+    </Tab.Navigator>
+);
+
+export default MyTabs;
+
